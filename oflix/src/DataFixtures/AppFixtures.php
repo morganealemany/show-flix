a13f34365199616b33105d04c07870cd66e8afaa
morganealemany <m.plancheron@gmail.com>
2021-08-23 17:38:28 +0200

    Installation de FakerPHP et utilisation sur les characters et tvshows

--- oflix/src/DataFixtures/AppFixtures.php
+++ oflix/src/DataFixtures/AppFixtures.php
@@ -7,17 +7,22 @@ use App\Entity\TvShow;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // Création de l'instance Faker Generator
+        $faker = Faker\Factory::create();
+
+
         // create 20 tvshows
         for ($i= 0; $i < 20 ; $i++) {
             $tvShow = new TvShow();
-            $tvShow->setTitle('Titre série ' .$i);
-            $tvShow->setSynopsis("Dans le futur, les gens peuvent passer un test qui pourrait leur permettre de vivre dans un monde appelé la Haute Mer. Ce monde se définit comme un paradis, mais seulement 3 % des candidats seront retenus.");
-            $tvShow->setNbLikes(150000);
+            $tvShow->setTitle($faker->sentence($nbWords = 4, $variableNbWords = true));
+            $tvShow->setSynopsis($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $tvShow->setNbLikes($faker->randomNumber());
             $tvShow->setPublishedAt(new DateTimeImmutable());
             $tvShow->setCreatedAt(new DateTimeImmutable());
 
@@ -49,8 +54,8 @@ class AppFixtures extends Fixture
         // create 20 characters! Bam!
         for ($i = 0; $i < 20; $i++) {
             $character = new Character();
-            $character->setFirstname('Prénom Perso '.$i);
-            $character->setLastname('Nom Perso '.$i);
+            $character->setFirstname($faker->firstName());
+            $character->setLastname($faker->lastName());
             $character->setGender(mt_rand(0, 1) ? 'Homme' : 'Femme');
             $character->setCreatedAt(new DateTimeImmutable());
             $manager->persist($character);
@@ -58,5 +63,6 @@ class AppFixtures extends Fixture
 
         $manager->flush();
     }
+
 }
 
