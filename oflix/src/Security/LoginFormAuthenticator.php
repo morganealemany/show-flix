829cbdb720a64198b4cd9dd4b320c25530a1ad6d
morganealemany <m.plancheron@gmail.com>
2021-08-29 12:12:06 +0200

    Ajout de l'email de l'utilisateur lorsque connecté sur la page d'accueil du backoffice

--- oflix/src/Security/LoginFormAuthenticator.php
+++ oflix/src/Security/LoginFormAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('index'));
+        return new RedirectResponse($this->urlGenerator->generate('backoffice'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
