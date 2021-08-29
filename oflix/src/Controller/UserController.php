2a4de0aed3351a060b48e8f955cc1034e226c274
morganealemany <m.plancheron@gmail.com>
2021-08-29 12:01:27 +0200

    Ajout du hashage pour le mot de passe lors de la création d'un nouveau compte. Problème sur l'édition d'un compte : Expected an instance of SymfonyComponentSecurityCoreUserUserInterface as first argument, but got string.

--- oflix/src/Controller/UserController.php
+++ oflix/src/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -28,13 +29,19 @@ class UserController extends AbstractController
     /**
      * @Route("/new", name="user_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //On hashe le mot de passe saisi en utilisant l'injection de UserPasswordHasherInterface dans la méthode
+            $user->setPassword($passwordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            ));
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
@@ -61,12 +68,19 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, User $user, UserPasswordHasherInterface $passwordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // On hashe le nouveau mot de passe avant de l'enregistrer en BDD
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    'user',
+                    $form->get('password')->getData()
+                )
+                );
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
