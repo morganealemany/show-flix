f94e3fcf69f87edbdb3f7a174d2d9d8501fe8efa
morganealemany <m.plancheron@gmail.com>
2021-08-29 11:23:16 +0200

    Création du CRUD pour l'entité User + message d'erreur: array to string conversion. Fix it avec changement de type pour la propriété role de l'entité User (passée de json à array)

--- oflix/src/Entity/User.php
+++ oflix/src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="array")
      */
     private $roles = [];
 
