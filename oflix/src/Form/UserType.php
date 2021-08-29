ba1cbd8ff135d3b8b95cbf6112984ee53fab2eda
morganealemany <m.plancheron@gmail.com>
2021-08-29 11:45:14 +0200

    Ajout du type PasswordType au champ password du form User pour cacher le mot de passe lors de la saisie ou de la modification d'un compte

--- oflix/src/Form/UserType.php
+++ oflix/src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,7 +24,7 @@ class UserType extends AbstractType
                 'multiple' => true, // choix multiple
                 // https://www.developpez.net/forums/d1860599/php/bibliotheques-frameworks/symfony/formulaire-ne-s-affiche-error-array-to-string-conversion-sf4/
             ])
-            ->add('password')
+            ->add('password', PasswordType::class)
         ;
     }
 
