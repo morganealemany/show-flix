9ff9439d9b23ee1a1ee4857b923a6fa72164fb77
morganealemany <m.plancheron@gmail.com>
2021-08-29 10:47:29 +0200

    Création compte user + essai connexion/déconnexion + ajout bouton dynamique dans la nav Connexion/Déconnexion

--- oflix/src/Security/LoginFormAuthenticator.php
+++ oflix/src/Security/LoginFormAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('index'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
