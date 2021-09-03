64bc3c646e4a9bd8d6e2571bb96479fde8fabad2
morganealemany <m.plancheron@gmail.com>
2021-09-03 18:28:20 +0200

    Ajout d'une variable d'environnement dans le fichier .env.local, utilisation de la variable en cours WIP

--- oflix/src/EventSubscriber/MaintenanceSubscriber.php
+++ oflix/src/EventSubscriber/MaintenanceSubscriber.php
@@ -9,26 +9,29 @@ use Symfony\Component\HttpKernel\Event\ResponseEvent;
 class MaintenanceSubscriber implements EventSubscriberInterface
 {
     public function onKernelResponse(ResponseEvent $event)
-    {
-        // On récupère la réponse de l'event
-        $response = $event->getResponse();
-        // On récupère son contenu HTML
-        $content = $response->getContent();
-        // dd($response, $content);
-        // On créé le texte voulu dans le contenu grâce à str_replace
-        $newBody = str_replace("<body>", "<body><div class=\"alert alert-danger\">Maintenance prévue mardi 7 septembre à 17h00</div>", $content);
-        // Puis on l'enregistre dans la variable $content
-        $newResponse = $response->setContent($newBody);
-        // Enfin on affecte les changements à response
-        $event->setResponse($newResponse);
-        // return $response->getContent();
-        // dd($newResponse);
+    {     
+            // On récupère la réponse de l'event
+            $response = $event->getResponse();
+            // On récupère son contenu HTML
+            $content = $response->getContent();
+            // dd($response, $content);
+            // On créé le texte voulu dans le contenu grâce à str_replace
+            $newBody = str_replace("<body>", "<body><div class=\"alert alert-danger\">Maintenance prévue mardi 7 septembre à 17h00</div>", $content);
+            // Puis on l'enregistre dans la variable $content
+            $newResponse = $response->setContent($newBody);
+            // Enfin on affecte les changements à response
+            $event->setResponse($newResponse);
+            // return $response->getContent();
+            // dd($newResponse);
+    
     }
 
     public static function getSubscribedEvents()
     {
-        return [
+        if ($_ENV['IN_MAINTENANCE']) {
+            return [
             'kernel.response' => 'onKernelResponse',
         ];
+        }
     }
 }
